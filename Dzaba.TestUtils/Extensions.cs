8939fd71364f7706498b17d9bea75f4d4dd9ab73
Dzaba <blazej.zabialowicz@gmail.com>
2019-11-18 11:40:31 +0100

    TestUtils

---
+++ Dzaba.TestUtils/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture;
+using Dzaba.Utils;
+using Moq;
+
+namespace Dzaba.TestUtils
+{
+    public static class Extensions
+    {
+        public static Mock<T> FreezeMock<T>(this IFixture fixture)
+            where T : class
+        {
+            Require.NotNull(fixture, nameof(fixture));
+
+            return fixture.Freeze<Mock<T>>();
+        }
+    }
+}
